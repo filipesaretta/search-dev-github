fc7fb2d54a413f5bb570fb4b3111f776519fd6d5
filipesaretta <filipessm124@gmail.com>
2022-04-04 11:11:15 -0300

    Add new fields

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,13 +1,23 @@
 import axios from 'axios';
 import { ChangeEvent, useEffect, useState } from 'react';
+import { AiOutlineLink, AiOutlineTwitter } from 'react-icons/ai';
+import { ImLocation } from 'react-icons/im';
+
 import search from '../../assets/search.svg';
-import { Card, CardDetails, Container } from './styles';
+import {
+  Card,
+  CardContainer,
+  CardDetails,
+  CardLinks,
+  CardMain,
+  CardUserImage,
+  Container,
+} from './styles';
 
 interface userProps {
   name: string;
   avatar_url: string;
   bio?: string;
-  created_at: string;
   email?: string;
   following: number;
   gravatar_id: string;
@@ -21,7 +31,6 @@ interface userProps {
   twitter_username?: string;
   followers?: string;
 }
-
 export function Search() {
   const [user, setUser] = useState<userProps | null>(null);
   const [getUser, setGetUser] = useState('');
@@ -40,10 +49,11 @@ export function Search() {
 
   function handleSearchUser() {
     setSearchUser(getUser);
+    setGetUser('');
   }
 
   function handleFindUser(e: ChangeEvent<HTMLInputElement>) {
-    const { value } = e.target as HTMLInputElement;
+    const { value } = e.target;
     setGetUser(value);
   }
 
@@ -57,6 +67,7 @@ export function Search() {
           type="text"
           placeholder="Search a GitHub username..."
           onChange={handleFindUser}
+          value={getUser}
         />
         <button type="submit" onClick={handleSearchUser}>
           Search
@@ -64,23 +75,61 @@ export function Search() {
       </Container>
 
       {user && (
-        <Card>
-          <h3>{user?.name}</h3>
-          <p>{user?.login}</p>
-          <p>{user?.bio ? user?.bio : 'This profile has no bio'}</p>
-          <CardDetails>
-            <div>
-              <p>Public Repos </p>
-              <p>{user?.public_repos}</p>
-            </div>
-            <div>
-              <p>Followers</p> <p> {user?.followers}</p>
-            </div>
-            <div>
-              <p>Following </p> <p>{user?.following}</p>
-            </div>
-          </CardDetails>
-        </Card>
+        <CardContainer>
+          <CardUserImage>
+            <img src={user.avatar_url} alt="Some user from github" />
+          </CardUserImage>
+          <CardMain>
+            <h3>{user?.name}</h3>
+            <a href={user.html_url}>@{user?.login}</a>
+            <p>{user?.bio ? user?.bio : 'This profile has no bio'}</p>
+          </CardMain>
+          <Card>
+            <CardDetails>
+              <div>
+                <p>Public Repos </p>
+                <p className="card-numbers">{user?.public_repos}</p>
+              </div>
+              <div>
+                <p>Followers</p>
+                <p className="card-numbers"> {user?.followers}</p>
+              </div>
+              <div>
+                <p>Following </p>
+                <p className="card-numbers">{user?.following}</p>
+              </div>
+            </CardDetails>
+            <CardLinks>
+              <p className="location">
+                <span>
+                  <ImLocation />
+                </span>
+                {user.location}
+              </p>
+              <a href={user.html_url} className="main-link">
+                <span>
+                  <AiOutlineLink />
+                </span>
+                Site
+              </a>
+              {user.twitter_username !== null ? (
+                <a href={`https://twitter.com/${user.twitter_username}`}>
+                  <span>
+                    <AiOutlineTwitter />
+                  </span>
+                  {user.twitter_username}
+                </a>
+              ) : (
+                <p>
+                  <span>
+                    <AiOutlineTwitter />
+                  </span>
+                  Not Available
+                </p>
+              )}
+            </CardLinks>
+          </Card>
+        </CardContainer>
       )}
     </>
   );
